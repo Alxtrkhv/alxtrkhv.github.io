2f3643ca052eb730e0edde358061d01cf1c40332
Alexander Terekhov <alxtrkhv@gmail.com>
2023-06-22 00:32:31 +0300

    wip: card

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,3 +1,16 @@
 export default function Main() {
-  return <div className="h-screen bg-stone-700"></div>;
+  return (
+    <div className="flex flex-col h-screen justify-center items-center bg-slate-800">
+      <Card />
+    </div>
+  );
+}
+
+function Card() {
+  return (
+    <div className="bg-slate-600 h-60 w-96 rounded-3xl shadow-2xl items-center flex flex-col text-slate-300">
+      <div className="mt-5">TITLE</div>
+      <div className="my-auto ">CONTENT</div>
+    </div>
+  );
 }
