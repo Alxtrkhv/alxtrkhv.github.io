d70da1cc70c0cfa3659d39734757a1ff9aadf28c
Alexander Terekhov <alxtrkhv@gmail.com>
2023-06-21 21:45:29 +0300

    refactor: project structure

--- src/components/ProfileCard/LinksPanel.tsx
+++ src/components/ProfileCard/LinksPanel.tsx
@@ -9,9 +9,9 @@ type Props = {
 export default function LinksPanel(props: Props) {
   return (
     <ul className={styles.links}>
-      {props.links.map((item) => (
-        <li>
-          <LinkButton icon={item[0]} link={item[1]} />
+      {props.links.map((item, index) => (
+        <li key={index}>
+	  <LinkButton icon={item[0]} link={item[1]} />
         </li>
       ))}
     </ul>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 
-import "./styles.css";
+import "../styles.css";
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
